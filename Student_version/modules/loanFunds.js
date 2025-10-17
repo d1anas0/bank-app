80d31b387b27129ab5607f2897f4a80426bb2600
d1anas0 <231689@gmail.com>
2025-10-17 11:23:47 +1100

    Chore: improve readability, maintainability within the loan feature.

--- Student_version/modules/loanFunds.js
+++ Student_version/modules/loanFunds.js
@@ -9,16 +9,22 @@ export const loanRequest = () => {
     btnLoan.addEventListener('click', e => {
         e.preventDefault()
 
-        // variables
-        const requestedAmount = Number(inputLoanAmount.value)
+        // variables & definitions
         const transactions = currentAccount.movements
+        const requestedAmount = Number(inputLoanAmount.value)
         const minimumDepositedAmount = requestedAmount * 0.1
         
+        const eligibleDeposit = (deposit) => deposit >= minimumDepositedAmount;
+        const loanReject = () => alert("Unfortunately the account does not meet our loan requirements, and your loan request has been rejected.")
+        const loanSuccess = () => transactions.push(requestedAmount) && alert('Your request for a loan of funds has been SUCCESSFUL, the requested amount will be deposited to your account within 3 business days.')
+        
         // condition 
-        const eligibleDepositVerification = transactions.some(deposit => deposit >= minimumDepositedAmount);
-
+        const eligibleDepositVerification = transactions.some(eligibleDeposit);
+        
         // business logic 
-       requestedAmount > 0 && eligibleDepositVerification ? transactions.push(requestedAmount) && alert('Your request for a loan of funds has been SUCCESSFUL, the requested amount will be deposited to your account within 3 business days.'): alert("Unfortunately the account does not meet our loan requirements, and your loan request has been rejected.")
+        requestedAmount > 0 && eligibleDepositVerification 
+       ? loanSuccess()
+       : loanReject()
 
         // update UI
         updateUI(currentAccount) 
